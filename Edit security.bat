be571c48dc6f223805cc31b55acb12c8578312bb
allanrasmussendk <128977731+allanrasmussendk@users.noreply.github.com>
2023-03-26 14:48:45 +0200

    Add files via upload

---
+++ Edit security.bat
@@ -0,0 +1,30 @@
+@echo off
+REM This is a script that uses a odd bug in the NTFS file system running on Windows,
+REM     to hide data from easy read without the "password".
+REM Note: Deleting the created Data.txt file (Which doesn't contain the hidden data), 
+REM     will also delete the hidden data (apparently).
+setlocal
+SET /P PASSWORD="Enter password: "
+if not exist Data.txt (
+    REM Password is not set => Create new file with entered password
+    echo No secret here > Data.txt
+    echo This is "Secure" data > Data.txt:%PASSWORD%.txt
+    notepad Data.txt:%PASSWORD%.txt
+    goto END
+)
+
+SET WRONG=Wrong password!!!
+SET DATA=%WRONG%
+SET /P DATA= < Data.txt:%PASSWORD%.txt
+if "[%DATA%]"=="[%WRONG%]" (
+    REM cls is a hack to remove "Den angivne fil blev ikke fundet." (Danish locale) error message. 2>nul can't be used.
+    cls
+    echo Wrong password
+    pause
+    goto END
+)
+
+start notepad Data.txt:%PASSWORD%.txt
+
+:END
+endlocal
